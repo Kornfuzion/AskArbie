181e976368d8c08b29cdcb656baad46c4ebd7e35
James Shen <james@ubuntu.(none)>
2014-06-07 11:51:24 -0700

    added database models

--- invest/models.py
+++ invest/models.py
@@ -5,3 +5,27 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     account_number = models.CharField(max_length=50)
+
+class RBC_Customer(models.Model):
+
+    userName = models.CharField(max_length=200)
+    income = models.PositiveIntegerField(u'income')
+    
+    investmentData_1 = models.FloatField(u'i_1')
+    investmentData_2 = models.FloatField(u'i_2')
+    investmentData_3 = models.FloatField(u'i_3')
+    investmentData_4 = models.FloatField(u'i_4')
+    investmentData_5 = models.FloatField(u'i_5')
+    investmentData_6 = models.FloatField(u'i_6')
+    investmentData_7 = models.FloatField(u'i_7')
+    investmentData_8 = models.FloatField(u'i_8')
+    investmentData_9 = models.FloatField(u'i_9')
+    investmentData_10 = models.FloatField(u'i_10')
+    investmentData_11 = models.FloatField(u'i_11')
+    investmentData_12 = models.FloatField(u'i_12')
+    investmentData_13 = models.FloatField(u'i_13')
+    investmentData_14 = models.FloatField(u'i_14')
+    investmentData_15 = models.FloatField(u'i_15')
+    
+    clusterID=models.PositiveIntegerField(u'clusterID')
+
