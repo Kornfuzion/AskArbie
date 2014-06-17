80724287d766b306f8d13d9c7e6c5b7973a0e1bc
TomZhangUT <tomzhang94@gmail.com>
2014-06-16 23:32:59 -0400

    cleaned up files

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/james/mysite/mydb',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/tom/code/mysite/mydb',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/james/mysite/media'
+MEDIA_ROOT = '/home/tom/code/mysite/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/james/mysite/static'
+STATIC_ROOT = '/home/tom/code/mysite/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
